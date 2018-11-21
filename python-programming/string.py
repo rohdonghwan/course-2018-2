bd14de6a18841dfbde0fe4b0b1b6d678b46e98dd
동환 노 <laborh0129@cu.ac.kr>
2018-11-21 15:15:39 +0900

    add string.py

--- python-programming/string.py
+++ python-programming/string.py
@@ -2,12 +2,15 @@ string = str(input("문자열을 입력 해 주세요"))
 strAry = ([])
 curChar = ""
 count = 0
+first = 1
 for i in string:
     strAry.append(i)
-print(len(strAry))
 for i in strAry:
     if curChar == i:
-        count = count + 1
+        if count == 0:
+            count = count + 2
+        else:
+            count = count + 1
     else:  
         if count != 0:
             print(curChar,count)
