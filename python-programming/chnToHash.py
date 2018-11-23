60b232ba8e6d0bbd2610374608806ccf8e9a239e
동환 노 <laborh0129@cu.ac.kr>
2018-11-23 14:16:34 +0900

    add

--- python-programming/chnToHash.py
+++ python-programming/chnToHash.py
@@ -1,10 +1,20 @@
 import re
 
 data = """
-park 010-9999-9988
-kim 010-9909-7789
-lee 010-8789-7768
+park@naver.com
+kim@daum.net
+lee@myhome.co.kr
 """
 
-dat = re.compile("(\d{3})[-](\d{4})[-](\d{4})")
-print(dat.sub("\g<1>-\g<2>-####",data))
+dat = re.compile(".*[@].*[.](?:com$|net$).*$")
+
+data2 = """
+kim,fail,fail
+shin,pass,fail
+"""
+
+dat2 = re.compile("(\w{3})[,](\w{4})[,](\w{4})")
+print(dat2.sub("\g<1>,\g<2>,pass",data2))
+
+dat2 = re.compile("fail$",re.MULTILINE)
+print(dat2.sub('pass',data2))
