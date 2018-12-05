28ed36ec95ffec847f3c8d5d69360d9ff0d13208
동환 노 <laborh0129@cu.ac.kr>
2018-12-05 16:45:12 +0900

    practiceProb-lynx.py

--- python-programming/practiceProb-lynx.py
+++ python-programming/practiceProb-lynx.py
@@ -1,4 +1,9 @@
 import pandas as pd
-lynx = pd.read_csv("lynx.csv", index_col = ["Year"])
+lynx = pd.read_csv("lynx.csv")
 print(lynx)
-lynx.dropna(axis = 1)
+lynx["Year"] = lynx.Year/10
+lynx["Year"] = lynx["Year"].round()*10
+print(lynx)
+sum_lynx = lynx.groupby("Year").sum()
+result = sum_lynx.sort_values("hunted",ascending = False)
+print(result)
